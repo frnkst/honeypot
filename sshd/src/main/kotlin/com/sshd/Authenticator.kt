c633085e3e298e08ec50f19663d2203580895d2b
Frank <frank@void0.ch>
2021-10-24 22:15:16 +0200

    Log ip address of server session

--- sshd/src/main/kotlin/com/sshd/Authenticator.kt
+++ sshd/src/main/kotlin/com/sshd/Authenticator.kt
@@ -1,8 +1,6 @@
 package com.sshd
 
-import org.apache.sshd.server.auth.AsyncAuthException
 import org.apache.sshd.server.auth.password.PasswordAuthenticator
-import org.apache.sshd.server.auth.password.PasswordChangeRequiredException
 import org.apache.sshd.server.session.ServerSession
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -10,20 +8,16 @@ import org.apache.kafka.clients.producer.ProducerRecord
 
 class Authenticator : PasswordAuthenticator {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
+    private val producer = createProducer()
 
-    private val producer = createProducer("localhost:29092")
-
-    @Throws(PasswordChangeRequiredException::class, AsyncAuthException::class)
     override fun authenticate(username: String?, password: String?, session: ServerSession?): Boolean {
         logger.debug("Login attempt by user $username with password $password")
-
-        val loginEvent = LoginEvent("192.168.0.1", username!!, password!!)
+        val loginEvent = LoginEvent(session?.ioSession?.remoteAddress.toString(), username!!, password!!)
         val loginEventJson = jsonMapper.writeValueAsString(loginEvent)
-
         val futureResult = producer.send(ProducerRecord("login", loginEventJson))
         futureResult.get()
 
-        return false // Authentication with our dummy server always fails
+        return false
     }
 }
 
--- sshd/src/main/kotlin/com/sshd/KafkaProducer.kt
+++ sshd/src/main/kotlin/com/sshd/KafkaProducer.kt
@@ -5,9 +5,9 @@ import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.common.serialization.StringSerializer
 import java.util.*
 
-fun createProducer(brokers: String): Producer<String, String> {
+fun createProducer(): Producer<String, String> {
     val props = Properties()
-    props["bootstrap.servers"] = brokers
+    props["bootstrap.servers"] = "localhost:29092"
     props["key.serializer"] = StringSerializer::class.java.canonicalName
     props["value.serializer"] = StringSerializer::class.java.canonicalName
     return KafkaProducer<String, String>(props)
