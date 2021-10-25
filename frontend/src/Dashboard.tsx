c8112a57b19f241b3a6d0d62e9715d8a417f2a9f
Frank <frank@void0.ch>
2021-10-25 22:38:58 +0200

    Cleanup

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,72 +1,11 @@
-import React, {MutableRefObject, useEffect, useRef, useState} from 'react'
-import {SSEProvider, useSSE} from "react-hooks-sse";
-
-export type LoginEvent = {
-	ip: string
-	user: string
-	password: string
-}
-
-const loginEvents: LoginEvent[]= [];
-
-const Comments = () => {
-	const [listening, setListening] = useState(false);
-	const [data, setData] = useState<LoginEvent[]>([]);
-	let eventSource: EventSource | undefined = undefined;
-
-	useEffect(() => {
-		if (!listening) {
-			eventSource = new EventSource("http://localhost:8080/events/login");
-
-			eventSource.onopen = (event) => {
-				console.log("connection opened")
-			}
-
-			eventSource.onmessage = (event) => {
-				console.log("result", event.data);
-				setData(old => [...old, event.data])
-			}
-
-			/*
-			eventSource.onerror = (event) => {
-				console.log(event.target.readyState)
-				if (event.target.readyState === EventSource.CLOSED) {
-					console.log('eventsource closed (' + event.target.readyState + ')')
-				}
-				eventSource.close();
-			}
-			 */
-
-			setListening(true);
-		}
-
-		return () => {
-			eventSource?.close();
-			console.log("eventsource closed")
-		}
-
-	}, [])
-
-	return (
-			<>
-					Received Data
-					{data.map((d, i) =>
-							<span key={i}>{d}</span>
-					)}
-
-			</>
-	);
-};
-
+import React from 'react'
+import {LoginEvents} from "./components/LoginEvents";
 
 export const Dashboard = () => {
 	return (
 			<div style={{ "marginTop": "20px", "textAlign": "center" }}>
 				<h1>Dashboard</h1>
-
-				<SSEProvider endpoint="http://localhost:8080/events/login">
-					<Comments/>
-				</SSEProvider>
+					<LoginEvents/>
 			</div>
 	)
 }
---
+++ frontend/src/components/LoginEvents.tsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react";
+
+export type LoginEvent = {
+	ip: string
+	user: string
+	password: string
+}
+
+export const LoginEvents = () => {
+	const [listening, setListening] = useState(false);
+	const [data, setData] = useState<LoginEvent[]>([]);
+	let eventSource: EventSource;
+
+	useEffect(() => {
+		if (!listening) {
+			eventSource = new EventSource("http://localhost:8080/events/login");
+
+			eventSource.onopen = (event) => {
+				console.log("connection opened")
+			}
+
+			eventSource.onmessage = (event) => {
+				console.log("result", event.data);
+				setData(old => [...old, event.data])
+			}
+
+			eventSource.onerror = (event: any) => {
+				console.log(event.target.readyState)
+				if (event.target.readyState === EventSource.CLOSED) {
+					console.log('eventsource closed (' + event.target.readyState + ')')
+				}
+				eventSource.close();
+			}
+
+			setListening(true);
+		}
+
+		return () => {
+			eventSource?.close();
+			console.log("eventsource closed")
+		}
+
+	}, [])
+
+	return (
+			<>
+				Login Events
+				{data.map((d, i) =>
+						<p key={i}>{d}</p>
+				)}
+
+			</>
+	);
+};
