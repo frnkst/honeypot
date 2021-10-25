768f9e87c26bffa543e2ea41eec19893304f5b15
Frank <frank@void0.ch>
2021-10-25 22:30:35 +0200

    Works like a charm

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {MutableRefObject, useEffect, useRef, useState} from 'react'
 import {SSEProvider, useSSE} from "react-hooks-sse";
 
 export type LoginEvent = {
@@ -7,16 +7,55 @@ export type LoginEvent = {
 	password: string
 }
 
-
-const loginEvents: LoginEvent[] = [];
+const loginEvents: LoginEvent[]= [];
 
 const Comments = () => {
-	const state = useSSE<LoginEvent>('message', { ip: '', user: '', password: ''});
-	loginEvents.push(state)
+	const [listening, setListening] = useState(false);
+	const [data, setData] = useState<LoginEvent[]>([]);
+	let eventSource: EventSource | undefined = undefined;
 
-	return <>
-		{ loginEvents.map((event) => <><p>{ event.ip } { event.user } { event.password }</p></>) }
-		</>
+	useEffect(() => {
+		if (!listening) {
+			eventSource = new EventSource("http://localhost:8080/events/login");
+
+			eventSource.onopen = (event) => {
+				console.log("connection opened")
+			}
+
+			eventSource.onmessage = (event) => {
+				console.log("result", event.data);
+				setData(old => [...old, event.data])
+			}
+
+			/*
+			eventSource.onerror = (event) => {
+				console.log(event.target.readyState)
+				if (event.target.readyState === EventSource.CLOSED) {
+					console.log('eventsource closed (' + event.target.readyState + ')')
+				}
+				eventSource.close();
+			}
+			 */
+
+			setListening(true);
+		}
+
+		return () => {
+			eventSource?.close();
+			console.log("eventsource closed")
+		}
+
+	}, [])
+
+	return (
+			<>
+					Received Data
+					{data.map((d, i) =>
+							<span key={i}>{d}</span>
+					)}
+
+			</>
+	);
 };
 
 
