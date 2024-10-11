35a6db4051b85e40c5b712ff5ac4f0595bb4158f
Frank <frank@void0.ch>
2024-10-11 20:29:14 +0200

    fix styling

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {LiveAttacksComponent} from "../components/live-attacks/live-attacks.component";
-import {TopPasswordsComponent} from "../components/top-passwords/top-passwords.component";
 import {TopUsernamesComponent} from "../components/top-usernames/top-usernames.component";
 import {TopIpsComponent} from "../components/top-ips/top-ips.component";
 import {
